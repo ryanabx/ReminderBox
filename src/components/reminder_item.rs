2ad312630690b6bad00ae7122fbc0cab5c9fcb35
Ryan Brue <ryanbrue.dev@gmail.com>
2026-02-12 18:35:16 -0600

    Add a really sick looking reminder checkbox ;)

--- src/components/reminder_item.rs
+++ src/components/reminder_item.rs
@@ -28,14 +28,27 @@ pub fn ReminderWidget(reminder: Reminder) -> impl IntoView {
     let state_setter = use_context::<WriteSignal<UserData>>().expect("Could not find user data");
     view! {
         <div draggable=true class="flex flex-row space-x-2">
-            <input type="checkbox" class="reminder-checkbox" bind:checked=reminder.completed/>
+            <ReminderCheckbox completed={reminder.completed} />
             <p class="grow py-2 px-2">{reminder.title}</p>
-            // <input type="text" class="grow py-2 px-2" placeholder="Enter a reminder..." bind:value=reminder.title />
             <button type="button" class="remove-button w-6" on:click=move |_| {
                 state_setter.update(|state| {
                     state.reminders_list.remove_reminder(reminder.id);
                 });
             }>X</button>
         </div>
+    }
+}
+
+#[component]
+pub fn ReminderCheckbox(completed: RwSignal<bool>) -> impl IntoView {
+    view! {
+        <label class="flex items-center cursor-pointer space-x-3">
+        <input type="checkbox" class="hidden peer" bind:checked=completed />
+        <div class="w-6 h-6 rounded-full border-2 border-gray-400 peer-checked:bg-blue-500 peer-checked:border-blue-500 transition-colors duration-150 flex items-center justify-center">
+            <svg class="w-3 h-3 {} text-white transition-opacity" class:opacity-0=move || {!completed.get()} fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+            </svg>
+        </div>
+        </label>
     }
 }
