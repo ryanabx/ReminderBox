208e70d53467562687e24de2c8e7a7f5e4720b9b
Ryan Brue <ryanbrue.dev@gmail.com>
2026-03-01 18:41:08 -0600

    Add dark mode
    Signed-off-by: Ryan Brue <ryanbrue.dev@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,29 @@ import Header from './components/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReminderPage from './pages/ReminderPage';
 import AboutPage from './pages/AboutPage';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
 
 function App() {
   return (
     <React.Fragment>
-      <CssBaseline />
-      <>
-        <BrowserRouter basename="/ReminderBox/">
-          <Header />
-          <Routes>
-            <Route path="/" element={<ReminderPage />} />
-            <Route path="/about" element={<AboutPage />} />
-          </Routes>
-        </BrowserRouter>
-      </>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <>
+          <BrowserRouter basename="/ReminderBox/">
+            <Header />
+            <Routes>
+              <Route path="/" element={<ReminderPage />} />
+              <Route path="/about" element={<AboutPage />} />
+            </Routes>
+          </BrowserRouter>
+        </>
+      </ThemeProvider>
     </React.Fragment>
   )
 }
